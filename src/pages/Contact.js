99f4cf2a59475c8aac7d28f5cbd0354693b3cfb4
Hector Perio <77209383+calcshector@users.noreply.github.com>
2021-10-14 00:56:12 -0700

    initial upload
    initial upload

---
+++ src/pages/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ContactList from '../components/ContactList';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>At the moment, I work for Amazon.</h1>
+
+            <h2>My work e-mail is perio@amazon.com</h2>
+
+            <h2>If you can't stop thinking of bananas and smiley faces because you work there too, or just like to shop at Amazon.com, let me know. :)</h2>
+
+            <ContactList />
+
+
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ContactList from '../components/ContactList';
+import '../styles/Home.css'
+import spockHeadShot from '../assets/passport.jpg'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 id='name'>Hi there! Welcome to Hector Perio's Portfolio Page</h1>
+            <div className='home-container'>
+                <img
+                    className='spock-headshot'
+                    src={spockHeadShot}
+                    alt="Afro Spock"
+                />
+
+                <p className='bio-text'>Hector Perio is a Full Stack Software Engineer with a focused technical backend programming language in C++. In front end software development: React, JavaScript, HTML, node.js, Inquirer, and CSS.
+
+I also have passion for competitive gaming and e-sports. Many of us in the computer science field were taught to make logic-based games like chess, scrabble, and snakes and ladders. Those made a lasting impression on me.
+
+Currently, I play League of Legends and World of Warcraft when I have the time. In the past, I've played Hearthstone, Chess, and Street Fighter. Sometimes, I will watch the top professionals to keep up with the scenes in these games/titles.
+
+Last, but certainly not least, I like to cycle for commute and leisure to minimize my carbon footprint, and use vehicles free from fossil fuels. I try to make this world a better place than when I found it when I get the chance.</p>
+            </div>
+
+            <ContactList />
+        </div>
+    );
+};
+
+export default Home;
