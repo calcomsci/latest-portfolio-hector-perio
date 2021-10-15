92eb999af26c05e51aabe682e6f7cc03115ce81c
Hector Perio <77209383+calcshector@users.noreply.github.com>
2021-10-15 14:48:26 -0700

    Update Home.js
    changed biography paragraph

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,11 +14,11 @@ const Home = () => {
                     alt="Afro Spock"
                 />
 
-                <p className='bio-text'>Hector Perio is a Full Stack Software Engineer with a focused technical backend programming language in C++. In front end software development: React, JavaScript, HTML, node.js, Inquirer, and CSS.
+                <p className='bio-text'>My name is Hector Perio and I'm a Full Stack Software Engineer with a focused technical backend programming language in C++. In front end software development I use React, JavaScript, HTML, node.js, Inquirer, and CSS.
 
 I also have passion for competitive gaming and e-sports. Many of us in the computer science field were taught to make logic-based games like chess, scrabble, and snakes and ladders. Those made a lasting impression on me.
 
-Last, but certainly not least, I like to cycle for commute and leisure to minimize my carbon footprint, and use vehicles free from fossil fuels. I try to make this world a better place than when I found it when I get the chance.</p>
+Last, but certainly not least, I like to commute and leisure using vehicles with no carbon footprint and use vehicles free from fossil fuels. I try to make this world a better place than when I found it when I get the chance.</p>
             </div>
 
             <ContactList />
